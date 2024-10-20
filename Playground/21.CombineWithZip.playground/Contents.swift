a90804b81f96006fab44993b35abcd6850c4a5bb
Le Ngoc <nguyenngocle2408@gmail.com>
2024-10-20 10:37:47 +0700

    - Syntax of the Zip function

--- Playground/21.CombineWithZip.playground/Contents.swift
+++ Playground/21.CombineWithZip.playground/Contents.swift
@@ -4,3 +4,10 @@ let sequence1 = [1, 2, 3, 5, 100, 2, 1000, -10]
 for (index, item) in zip(sequence1.indices, sequence1) {
     print("item by index: \(index) : \(item)")
 }
+print("----------------------------")
+let sequence2 = [1, 2, 3]
+let name = ["Nguyễn", "Ngọc", "Lễ"]
+let zipResult = zip(sequence2, name)
+for (index, item) in zipResult {
+    print("index and name: \(index) : \(item)")
+}
