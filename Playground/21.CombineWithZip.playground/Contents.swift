df29eeb03c5249880eefb0eab9e48fca65dd7ff3
Le Ngoc <nguyenngocle2408@gmail.com>
2024-10-20 14:38:41 +0700

    - Combine zip basic

--- Playground/21.CombineWithZip.playground/Contents.swift
+++ Playground/21.CombineWithZip.playground/Contents.swift
@@ -11,3 +11,15 @@ let zipResult = zip(sequence2, name)
 for (index, item) in zipResult {
     print("index and name: \(index) : \(item)")
 }
+print("--------------Deep Dive into the Zip Operator in Swift--------------")
+let publisher1 = PassthroughSubject<Int, Never>()
+let publisher2 = PassthroughSubject<String, Never>()
+let combinedPublisher = publisher1.zip(publisher2)
+    .sink { value in
+        print("Received pair: \(value)")
+    }
+publisher1.send(1)
+publisher2.send("Hello")
+publisher1.send(2)
+publisher2.send("World")
+combinedPublisher.cancel()
